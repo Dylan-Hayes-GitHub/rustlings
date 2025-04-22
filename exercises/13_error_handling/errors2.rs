444661713e1af8cb765eb9b18c82d9b518ee46ba
Dylan Hayes <dylanmhayes12@gmail.com>
2025-04-22 21:22:15 +0100

    error handling rust

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -20,10 +20,14 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
 
-    // TODO: Handle the error case as described above.
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    let result = match qty {
+        Ok(total) => total * cost_per_item + processing_fee,
+        Err(e) => return Err(e),
+    };
+
+    Ok(result)
 }
 
 fn main() {
